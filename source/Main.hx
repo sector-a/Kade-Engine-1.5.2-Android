c327f5fcdfa8f2c4f2ea062d314e1319ca93699e
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-08-11 23:10:02 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -79,6 +79,8 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 
+                SUtil.check();
+
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 
 		addChild(game);
